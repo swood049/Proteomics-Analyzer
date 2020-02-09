dfc2ca199b6ea5892cfd3af3b005026f3400d541
Steven Woodard <38500060+swood049@users.noreply.github.com>
2020-02-09 01:30:17 -0500

    Upload to github

---
+++ bioinformatics/bioinformatics/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace bioinformatics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputTextbox = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.negativeErrorsLabel = new System.Windows.Forms.Label();
+            this.positiveErrorsLabel = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.referenceTextBox = new System.Windows.Forms.TextBox();
+            this.tumorTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button1.Location = new System.Drawing.Point(308, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Compare the Proteins";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // outputTextbox
+            // 
+            this.outputTextbox.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.outputTextbox.Location = new System.Drawing.Point(190, 177);
+            this.outputTextbox.Name = "outputTextbox";
+            this.outputTextbox.ReadOnly = true;
+            this.outputTextbox.Size = new System.Drawing.Size(408, 204);
+            this.outputTextbox.TabIndex = 1;
+            this.outputTextbox.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(604, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(183, 167);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(11, 57);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(173, 167);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Positive";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(663, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Negative";
+            // 
+            // negativeErrorsLabel
+            // 
+            this.negativeErrorsLabel.AutoSize = true;
+            this.negativeErrorsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.negativeErrorsLabel.Location = new System.Drawing.Point(669, 125);
+            this.negativeErrorsLabel.Name = "negativeErrorsLabel";
+            this.negativeErrorsLabel.Size = new System.Drawing.Size(70, 25);
+            this.negativeErrorsLabel.TabIndex = 6;
+            this.negativeErrorsLabel.Text = "label3";
+            this.negativeErrorsLabel.Visible = false;
+            // 
+            // positiveErrorsLabel
+            // 
+            this.positiveErrorsLabel.AutoSize = true;
+            this.positiveErrorsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.positiveErrorsLabel.Location = new System.Drawing.Point(72, 127);
+            this.positiveErrorsLabel.Name = "positiveErrorsLabel";
+            this.positiveErrorsLabel.Size = new System.Drawing.Size(70, 25);
+            this.positiveErrorsLabel.TabIndex = 7;
+            this.positiveErrorsLabel.Text = "label4";
+            this.positiveErrorsLabel.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(190, 57);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(408, 114);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(638, 411);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // referenceTextBox
+            // 
+            this.referenceTextBox.Location = new System.Drawing.Point(12, 296);
+            this.referenceTextBox.Name = "referenceTextBox";
+            this.referenceTextBox.ReadOnly = true;
+            this.referenceTextBox.Size = new System.Drawing.Size(152, 20);
+            this.referenceTextBox.TabIndex = 10;
+            // 
+            // tumorTextBox
+            // 
+            this.tumorTextBox.Location = new System.Drawing.Point(12, 387);
+            this.tumorTextBox.Name = "tumorTextBox";
+            this.tumorTextBox.ReadOnly = true;
+            this.tumorTextBox.Size = new System.Drawing.Size(152, 20);
+            this.tumorTextBox.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Reference Sequence";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 368);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Tumor Sequence";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 322);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Change Reference";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 411);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Change Tumor";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(187, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(398, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Doctors and Researchers, before you hums a humble program to serve your needs.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(169, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(436, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = " Enter text files of a reference sequence of amino acids in a protein along with " +
+    "a suspicious,";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(49, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(690, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "homologous protein, and this program will alert you to function-imparing mutation" +
+    "s by through analysis of inherent electrical charges in the proteins.";
+            this.label7.Click += new System.EventHandler(this.Label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tumorTextBox);
+            this.Controls.Add(this.referenceTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.positiveErrorsLabel);
+            this.Controls.Add(this.negativeErrorsLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.outputTextbox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Proteins Project";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox outputTextbox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label negativeErrorsLabel;
+        private System.Windows.Forms.Label positiveErrorsLabel;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox referenceTextBox;
+        private System.Windows.Forms.TextBox tumorTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ bioinformatics/bioinformatics/Form1.cs
@@ -0,0 +1,210 @@
+﻿// Authors: Steven Woodard and Sambridee Mahat
+// Date: 8-Feb-2020
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace bioinformatics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            referenceTextBox.Text = "protein1.txt";
+            tumorTextBox.Text = "protein2.txt";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // take in two files as single strings
+            string proteinOne = File.ReadAllText(referenceTextBox.Text).ToLower();
+            string proteinTwo = File.ReadAllText(tumorTextBox.Text).ToLower();
+            // variable to hold all of the possible amino acid mutation location values max at 1000
+            int[] possibleWeaknessLocations = new int[1000];
+            //variable to count the mutations
+            int count = 0;
+            // more specific counters
+            int shouldBePositiveIsNegative = 0;
+            int shouldBeNegativeIsPositive = 0;
+            int shouldBePositiveIsNeutral = 0;
+            int shouldBeNeutralIsPositive = 0;
+            int shouldBeNegativeIsNeutral = 0;
+            int shouldBeNeutralIsNegative = 0;
+
+            //clear any previous entries
+            outputTextbox.Clear();
+
+            // loop through the characters in the string, and count the problems by looking at the charges
+            // and comparing the changes of the good file with the suspect file.
+            for (int i = 0; i < proteinOne.Length; i++)
+            {
+                // if the correct amino acids are positive, but the ones on file are negative, that's a problem.
+                // count that.
+                if((proteinOne[i] == 'k' || proteinOne[i] == 'r' || proteinOne[i] == 'h') && 
+                    (proteinTwo[i] == 'd' || proteinTwo[i] == 'e'))
+                {
+                    shouldBePositiveIsNegative++;
+                    possibleWeaknessLocations[count] = i;
+                    count++;
+                }
+                // if the correct amino acids are negative, but the ones of file are positive, that's a problem.
+                // count it.
+                else if((proteinOne[i] == 'd' || proteinOne[i] == 'e') &&
+                    (proteinTwo[i] == 'k' || proteinTwo[i] == 'r' || proteinTwo[i] == 'h'))
+                {
+                    shouldBeNegativeIsPositive++;
+                    possibleWeaknessLocations[count] = i;
+                    count++;
+                }
+                // if the correct amino acids are positive, but the ones on file are neutral, that's a big problem.
+                else if((proteinOne[i] == 'k' || proteinOne[i] == 'r' || proteinOne[i] == 'h') && 
+                    (proteinTwo[i] == 'a' || proteinTwo[i] == 'g' || proteinTwo[i] == 'v' || proteinTwo[i] == 'i' ||
+                    proteinTwo[i] == 'w' || proteinTwo[i] == 'f' || proteinTwo[i] == 'p' || proteinTwo[i] == 'm'||
+                        proteinTwo[i] == 'l'))
+                {
+                    shouldBePositiveIsNeutral++;
+                    possibleWeaknessLocations[count] = i;
+                    count++;
+                }
+                // if the correct amino acids are neutral, but the ones on file are positive, that's a big problem.
+                else if((proteinOne[i] == 'a' || proteinOne[i] == 'g' || proteinOne[i] == 'v' || proteinOne[i] == 'i' ||
+                    proteinOne[i] == 'w' || proteinOne[i] == 'f' || proteinOne[i] == 'p' || proteinOne[i] == 'm' ||
+                        proteinOne[i] == 'l') && (proteinTwo[i] == 'k' || proteinTwo[i] == 'r' || proteinTwo[i] == 'h'))
+                {
+                    shouldBeNeutralIsPositive++;
+                    possibleWeaknessLocations[count] = i;
+                    count++;
+                }
+                // if the correct amino acids are negative, but the ones of file are neutral, that's a problem (big).
+                else if((proteinOne[i] == 'd' || proteinOne[i] == 'e') && (proteinTwo[i] == 'a' || proteinTwo[i] == 'g' || proteinTwo[i] == 'v' || proteinTwo[i] == 'i' ||
+                    proteinTwo[i] == 'w' || proteinTwo[i] == 'f' || proteinTwo[i] == 'p' || proteinTwo[i] == 'm' ||
+                        proteinTwo[i] == 'l'))
+                {
+                    shouldBeNegativeIsNeutral++;
+                    possibleWeaknessLocations[count] = i;
+                    count++;
+                }
+                // if the correct amino acids are neutral, but the ones on file are negative, that's a problem.
+                else if((proteinOne[i] == 'a' || proteinOne[i] == 'g' || proteinOne[i] == 'v' || proteinOne[i] == 'i' ||
+                    proteinOne[i] == 'w' || proteinOne[i] == 'f' || proteinOne[i] == 'p' || proteinOne[i] == 'm' ||
+                        proteinOne[i] == 'l') && (proteinTwo[i] == 'd' || proteinTwo[i] == 'e'))
+                {
+                    shouldBeNeutralIsNegative++;
+                    possibleWeaknessLocations[count] = i;
+                    count++;
+                }
+
+            }
+            // display the problems or lack thereof.
+            if (count > 1)
+            {
+                outputTextbox.Text = "There are " + count + " serious mutations in this protein.\n";
+            }
+            else
+            {
+                outputTextbox.Text += "There are " + count + " serious mutations in this protein.\n";
+                outputTextbox.Text += "YAY!!!!!";
+            }
+            if (shouldBePositiveIsNegative >= 1)
+            {
+                outputTextbox.Text += "There is/are " + shouldBePositiveIsNegative + " amino acids in the mutated protein that is/are negative and should be positive.\n";
+            }
+            if(shouldBeNegativeIsPositive >= 1)
+            {
+                outputTextbox.Text += "There is/are " + shouldBeNegativeIsPositive + " amino acids in the mutated protein that is/are positive and should be negative.\n";
+            }
+            if (shouldBePositiveIsNeutral >= 1)
+            {
+                outputTextbox.Text += "There is/are " + shouldBePositiveIsNeutral + " amino acids in the mutated protein that shoud be positive but is/are neutral.\n";
+            }
+            if (shouldBeNeutralIsPositive >= 1)
+            {
+                outputTextbox.Text += "There is/are " + shouldBeNeutralIsPositive + " amino acids in the mutated protein that should be neutral but is/are positive.\n";
+            }
+            if(shouldBeNegativeIsNeutral >= 1)
+            {
+                outputTextbox.Text += "There is/are " + shouldBeNegativeIsNeutral + " amino acids in the mutated protein that should be negative but is/are neutral.\n";
+            }
+            if (shouldBeNeutralIsNegative >= 1)
+            {
+                outputTextbox.Text += "There is/are " + shouldBeNeutralIsNegative + " amino acids in the mutated protein that should be neutral but is/are negative.\n";
+            }
+            // Make it the output 'pop'
+            pictureBox2.Visible = true;
+            pictureBox1.Visible = true;
+            positiveErrorsLabel.Text = shouldBeNeutralIsPositive + shouldBeNegativeIsPositive + "";
+            negativeErrorsLabel.Text = shouldBeNeutralIsNegative + shouldBePositiveIsNegative + "";
+            positiveErrorsLabel.Visible = true;
+            negativeErrorsLabel.Visible = true;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                referenceTextBox.Text = openFileDialog1.FileName;
+
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog2 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                tumorTextBox.Text = openFileDialog2.FileName;
+            }
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
